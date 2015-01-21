8811cfa4979349ff32c4161bd5df47c7fc9679db
Babatope <babathurpe@yahoo.com>
2015-01-21 16:02:24 -0500

    i think i finished a test...hurray!!!

--- test/cpd4414/assign1/OrderQueueTest.java
+++ test/cpd4414/assign1/OrderQueueTest.java
@@ -73,12 +73,21 @@ public class OrderQueueTest {
         order2.addPurchase(new Purchase("PROD0006", 250));
         orderQueue.add(order1);
         orderQueue.add(order2);
+        order1.setTimeReceived(new Date());
+        order2.setTimeReceived(new Date());
+        boolean earliestTimeReceived = false;
+        //System.out.println(order1.getTimeFulfilled().getTime());
+        long result1 = order1.getTimeReceived().getTime();
+        long result2 = order1.getTimeReceived().getTime();
         
+        if(result1 > result2){
+            order1.getListOfPurchases();
+        } else{
+            order2.getListOfPurchases();
+            earliestTimeReceived = true;
+        }
         
-        
-        long expResult = new Date().getTime();
-        long result = order1.getTimeReceived().getTime();
-        assertTrue(Math.abs(result - expResult) < 1000);
+        assertTrue(earliestTimeReceived);
     }
     
 }
